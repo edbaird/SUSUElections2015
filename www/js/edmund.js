6d6295d7f5b60182e8ea33c11b47a73b3da534cc
electronic Max <electronic@gmail.com>
2015-02-21 17:27:26 +0000

    code committed

---
+++ www/js/edmund.js
@@ -0,0 +1,29 @@
+// beautiful code by ed, max didn't have anything to do with it
+angular.module('susu', [])
+	.controller("elections", function($scope) {
+		var datadfd = $.get('data/pages.json'),
+			sa = function(fn) { 
+				setTimeout(function() { $scope.$apply(fn); },0);
+			};
+		$.when(datadfd, window.fbinit).then(function(data) { 
+			sa(function() { 
+				var positions = $scope.positions = data[0].positions;
+				sa(function() { 
+					FB.login(function(){
+						console.log('done login'); 
+						positions.map(function(pos) { 
+							console.log(pos);
+							pos.candidates.map(function(person) { 
+								// console.log('getting page info ', person.id);
+								FB.api(''+person.id, function(x, err) { 
+									console.log(x, err); 
+									sa(function() { person.pageinfo = x; });
+								})
+							});
+						});						
+					}, {});
+				});
+			}); 
+		}).fail(function(err) { console.log('error ', err); });
+		s = $scope;
+	});
