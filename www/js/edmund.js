6fa15d0fd0a1fb3b89198ca9d62c6f95b54f5414
Adam Thomas <adam.thomas@inqb8r.tv>
2015-02-24 01:12:58 +0000

    lets try this

--- www/js/edmund.js
+++ www/js/edmund.js
@@ -1,28 +1,41 @@
 angular.module('susu', [])
 	.controller("elections", function($scope) {
-		var datadfd = $.get('data/pages.json'),
-			sa = function(fn) { 
-				setTimeout(function() { $scope.$apply(fn); },0);
-			};
-		$.when(datadfd, window.fbinit).then(function(data) { 
-			sa(function() { 
+		var datadfd = $.get('data/pages.json');
+		$.when(datadfd).then(function(data) {
+			var positions = $scope.positions = data.positions;
+			positions.map(function(pos) {
+				pos.candidates.map(function(person) {
+
+					// Can I use this API in this way? Yolo swag. Adam woz ere lol 2k10.
+					if (person.id) {
+						$.getJSON('https://graph.facebook.com/'+person.id+'?fields=likes').then(function(res) {
+							person.pageinfo = res;
+							$scope.$apply();
+						});
+					}
+				});
+			});
+
+
+
+			/*sa(function() {
 				var positions = $scope.positions = data[0].positions;
-				sa(function() { 
+				sa(function() {
 					FB.login(function(){
-						console.log('done login'); 
-						positions.map(function(pos) { 
+						console.log('done login');
+						positions.map(function(pos) {
 							console.log(pos);
-							pos.candidates.map(function(person) { 
+							pos.candidates.map(function(person) {
 								// console.log('getting page info ', person.id);
-								FB.api(''+person.id, function(x, err) { 
-									console.log(x, err); 
+								FB.api(''+person.id, function(x, err) {
+									console.log(x, err);
 									sa(function() { person.pageinfo = x; });
 								})
 							});
-						});						
+						});
 					}, {});
 				});
-			}); 
+			}); */
 		}).fail(function(err) { console.log('error ', err); });
 		s = $scope;
 	});
